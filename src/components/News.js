875e294d9f3a59a97ffab5768770ae2c46118afe
vyshuepuri1520 <saivyshu1520@gmail.com>
2024-01-19 17:26:41 +0530

    completed33

--- src/components/News.js
+++ src/components/News.js
@@ -25,45 +25,59 @@ export class News extends Component {
       page : 1
     }
   }
-
-  async componentDidMount(){
-    console.log("cdm");
-    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=805dddd9d0b54ccdae554c5ec350b5b4&page=1&pageSize=${this.props.pageSize}`;
+  async updateNews(){
+    const url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=805dddd9d0b54ccdae554c5ec350b5b4&page=${this.state.page}&pageSize=${this.props.pageSize}`;
     this.setState({loading :true});
     let data = await fetch(url);
     let parsedData = await data.json();
-    console.log(parsedData);
     this.setState({articles:parsedData.articles,
       totalResults:parsedData.totalResults,
       loading : false
     })
   }
+  async componentDidMount(){
+    // console.log("cdm");
+    // let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=805dddd9d0b54ccdae554c5ec350b5b4&page=1&pageSize=${this.props.pageSize}`;
+    // this.setState({loading :true});
+    // let data = await fetch(url);
+    // let parsedData = await data.json();
+    // console.log(parsedData);
+    // this.setState({articles:parsedData.articles,
+    //   totalResults:parsedData.totalResults,
+    //   loading : false
+    // })
+    this.updateNews();
+  }
 
    handlePreviousClick=async()=>{
-    console.log("previous")
-    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=805dddd9d0b54ccdae554c5ec350b5b4&page=${this.state.page-1}&pageSize=${this.props.pageSize}`;
-    this.setState({loading :true});
-    let data = await fetch(url);
-    let parsedData = await data.json();
-      this.setState({
-      page : this.state.page-1,
-      articles:parsedData.articles,
-      loading : false
-    })
+    // console.log("previous")
+    // let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=805dddd9d0b54ccdae554c5ec350b5b4&page=${this.state.page-1}&pageSize=${this.props.pageSize}`;
+    // this.setState({loading :true});
+    // let data = await fetch(url);
+    // let parsedData = await data.json();
+    //   this.setState({
+    //   page : this.state.page-1,
+    //   articles:parsedData.articles,
+    //   loading : false
+    // })
+    this.setState({page:this.state.page-1});
+    this.updateNews();
   }
    handleNextClick=async()=>{
-    console.log("next")
-    if(!(this.state.page+1>Math.ceil(this.state.totalResults/this.props.pageSize)))
-    {
-    let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=805dddd9d0b54ccdae554c5ec350b5b4&page=${this.state.page+1}&pageSize=${this.props.pageSize}`;
-    this.setState({loading :true});
-    let data = await fetch(url);
-    let parsedData = await data.json();
-      this.setState({
-      page : this.state.page+1,
-      articles:parsedData.articles,
-      loading : false
-    })}
+    // console.log("next")
+    // if(!(this.state.page+1>Math.ceil(this.state.totalResults/this.props.pageSize)))
+    // {
+    // let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=805dddd9d0b54ccdae554c5ec350b5b4&page=${this.state.page+1}&pageSize=${this.props.pageSize}`;
+    // this.setState({loading :true});
+    // let data = await fetch(url);
+    // let parsedData = await data.json();
+    //   this.setState({
+    //   page : this.state.page+1,
+    //   articles:parsedData.articles,
+    //   loading : false
+    // })}
+    this.setState({page:this.state.page+1});
+    this.updateNews();
   }
   render() {
     console.log("render")
@@ -74,7 +88,7 @@ export class News extends Component {
         <div className="row">
         {!this.state.loading && this.state.articles.map((element)=>{
           return <div className="col-md-4" key = {element.url}>
-          <NewsItem  title={element.title?element.title.slice(0,30):""} description = {element.description?element.description.slice(0,90):""} imageUrl = {element.urlToImage} newsUrl = {element.url}/>
+          <NewsItem  title={element.title?element.title.slice(0,30):""} description = {element.description?element.description.slice(0,90):""} imageUrl = {element.urlToImage} newsUrl = {element.url} author = {element.author} date = {element.publishedAt} source = {element.source.name}/>
           </div>
           })}
         </div>
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,16 +2,17 @@ import React, { Component } from "react";
 
 export class NewsItem extends Component {
   render() {
-    let {title,description,imageUrl,newsUrl} = this.props;
+    let {title,description,imageUrl,newsUrl,author,date,source} = this.props;
     return (
       <div className="my-3">       
         <div className="card" >
-          <img src={imageUrl?imageUrl:"https://images.seattletimes.com/wp-content/uploads/2024/01/01092024_tzr_tzr_202530.jpg?d=1200x630"} className="card-img-top" alt="..." />
+        <span class="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style = {{left  : '90%' ,zIndex: '1' }}>{source}  </span>          <img src={imageUrl?imageUrl:"https://images.seattletimes.com/wp-content/uploads/2024/01/01092024_tzr_tzr_202530.jpg?d=1200x630"} className="card-img-top" alt="..." />
           <div className="card-body">
             <h5 className="card-title">{title}...</h5>
             <p className="card-text">
              {description}...
             </p>
+            <p className="card-text"><small className="text-danger">By {author?author:"Unknown"} on {new Date(date).toGMTString()} </small></p>
             <a rel="noreferrer" href={newsUrl} target = "_blank" className="btn btn-sm btn-info">
               Read More
             </a>
